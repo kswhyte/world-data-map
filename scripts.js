fe7d5721b0f258e80fccf51b7d58001e595f636d
KINAN WHYTE <kinanwhyte@gmail.com>
2016-12-18 20:41:44 -0700

    Add world-data-orthographic view of world

---
+++ scripts.js
@@ -0,0 +1,47 @@
+var map = new Datamap({
+  scope: 'world',
+  element: document.getElementById('container'),
+  projection: 'orthographic',
+  fills: {
+    defaultFill: "#dda3a3",
+    // gt50: colors(Math.random() * 20),
+    // eq50: colors(Math.random() * 20),
+    // lt25: colors(Math.random() * 10),
+    // gt75: colors(Math.random() * 200),
+    // lt50: colors(Math.random() * 20),
+    // eq0: colors(Math.random() * 1),
+    // pink: '#0fa0fa',
+    // gt500: colors(Math.random() * 1)
+  },
+  projectionConfig: {
+    rotation: [97,-30]
+    // rotation: [30],
+    // rotation: [30]
+  },
+  // data: {
+    // 'USA': {fillKey: 'lt50' },
+    // 'MEX': {fillKey: 'lt25' },
+    // 'CAN': {fillKey: 'gt50' },
+    // 'GTM': {fillKey: 'gt500'},
+    // 'HND': {fillKey: 'eq50' },
+    // 'BLZ': {fillKey: 'pink' },
+    // 'GRL': {fillKey: 'eq0' },
+    // 'CAN': {fillKey: 'gt50' }
+//   }
+// });
+
+// map.graticule();
+
+// map.arc([{
+//   origin: {
+//     latitude: 61,
+//     longitude: -149
+//   },
+//   destination: {
+//     latitude: -22,
+//     longitude: -43
+//   }
+// }], {
+//   greatArc: true,
+//   animationSpeed: 2000
+});
