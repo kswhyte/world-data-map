17c9e6de6e10766b4176866c545e31c28a8fa444
KINAN WHYTE <kinanwhyte@gmail.com>
2016-12-19 12:16:28 -0700

    Zoom based on coordinate

--- scripts.js
+++ scripts.js
@@ -18,6 +18,20 @@ var map = new Datamap({
     // rotation: [30],
     // rotation: [30]
   },
+  element: document.getElementById("container"),
+  scope: 'world',
+  // Zoom in on Africa
+  setProjection: function(element) {
+    var projection = d3.geo.equirectangular()
+      .center([23, -3])
+      .rotate([4.4, 0])
+      .scale(400)
+      .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+    var path = d3.geo.path()
+      .projection(projection);
+
+    return {path: path, projection: projection};
+  },
   // data: {
     // 'USA': {fillKey: 'lt50' },
     // 'MEX': {fillKey: 'lt25' },
